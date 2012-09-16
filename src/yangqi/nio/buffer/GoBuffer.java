f03434d6f22a90839d5cfa736585cd371936ee12
llohellohe <yangqichris@gmail.com>
2012-09-16 18:33:11 +0800

    add

--- src/yangqi/nio/buffer/GoBuffer.java
+++ src/yangqi/nio/buffer/GoBuffer.java
@@ -1,5 +1,6 @@
 package yangqi.nio.buffer;
 
+import java.nio.ByteOrder;
 import java.nio.CharBuffer;
 
 public class GoBuffer {
@@ -9,27 +10,38 @@ public class GoBuffer {
 	 */
 	public static void main(String[] args) {
 		testPutAndGet();
-		
+
+		System.out.println(ByteOrder.nativeOrder());
+
+		int i = 0;
+		System.out.println(i++);
+
 	}
-	
-	public static void testPutAndGet(){
-		CharBuffer charBuffer=CharBuffer.allocate(10);
-		
-		String a="hello";
-		
+
+	public static void testPutAndGet() {
+		int mark = 3;
+		CharBuffer charBuffer = CharBuffer.allocate(10);
+
+		System.out.println("IS DIRECT " + charBuffer.isDirect());
+		String a = "hello";
+
 		System.out.println(a);
-		for(int i=0;i<a.length();i++){
-		charBuffer.put(a.charAt(i));
+		for (int i = 0; i < a.length(); i++) {
+			if (i == 3) {
+				charBuffer.mark();
+			}
+			charBuffer.put(a.charAt(i));
 		}
 		charBuffer.flip();
-		
-			System.out.println(charBuffer.get());
-			System.out.println(charBuffer.get());
-			System.out.println(charBuffer.get());
-		while(charBuffer.hasRemaining( )){
+
+		System.out.println(charBuffer.get());
+		System.out.println(charBuffer.get());
+		System.out.println(charBuffer.get());
+		while (charBuffer.hasRemaining()) {
 			System.out.println(charBuffer.get());
 		}
-		
+		System.out.println("RESET");
+
 	}
 
 }
